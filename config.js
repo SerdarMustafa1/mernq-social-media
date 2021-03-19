80460a165fa2277d4aabac7a67e43b40efc05b9e
Serdar Mustafa <serdar.mustafa@rota.com>
2021-03-19 13:41:29 +0000

    social media app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://serdarmustafa1:Birgit2706!@cluster0.vtry6.mongodb.net/testDb?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
